7b6fa4c42c5e43b9189950cfc59cc5ab24c31c89
Orfeas <orfeas126@gmail.com>
2022-02-01 22:21:00 -0800

    Update .gitignore

--- boot.asm
+++ boot.asm
@@ -1,3 +1,25 @@
+;mov ah, 0x0e
+;mov al, 65
+;int 0x10
+;
+;loop:
+;        inc al
+;        cmp al, 'Z' + 1
+;        je exit
+;        int 0x10
+;        jmp loop
+;exit:
+;        jmp $
+
+[org 0x7c00]
+mov ah, 0x0e
+mov al, [varA]
+int 0x10
 jmp $
+
+varA:
+    db "Hello!", 0
+
+
 times 510-($-$$) db 0
 db 0x55, 0xaa
